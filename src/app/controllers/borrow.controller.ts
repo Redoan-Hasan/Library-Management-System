40e569a62721e5448301837fd7f47a3d6c1907b1
Redoan Hasan <redoanhasan57@gmail.com>
2025-06-22 23:39:07 +0600

    all done

--- src/app/controllers/borrow.controller.ts
+++ src/app/controllers/borrow.controller.ts
@@ -10,26 +10,74 @@ borrowRoutes.post(
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       const borrowData = await borrowZodSchema.parseAsync(req.body);
-      console.log("req.body",borrowData.dueDate);
+      console.log("req.body", borrowData.dueDate);
       const targetBook = await Book.findById(borrowData.book);
       console.log(targetBook);
-      if (targetBook && Number(targetBook.copies) > 0 && Number(targetBook.copies) >= borrowData.quantity && targetBook.available === true) {
+      if (
+        targetBook &&
+        Number(targetBook.copies) > 0 &&
+        Number(targetBook.copies) >= borrowData.quantity &&
+        targetBook.available === true
+      ) {
         targetBook.copies = Number(targetBook.copies) - borrowData.quantity;
         await targetBook.save();
         const borrow = await Borrow.create(borrowData);
-          await targetBook.updateBookAvailability();
+        await targetBook.updateBookAvailability();
         res.status(201).send({
-        success: true,
-        message: "Book borrowed successfully",
-        data: borrow,
-      });
+          success: true,
+          message: "Book borrowed successfully",
+          data: borrow,
+        });
       } else {
         res.status(400).send({
-          message :"Something went wrong ! please check the book id and availability of book you want to borrow or contact admin for more information!",
-          success:false
-        })
+          message:
+            "Something went wrong ! please check the book id and availability of book you want to borrow or contact admin for more information!",
+          success: false,
+        });
       }
-      
+    } catch (error: any) {
+      next(error);
+    }
+  }
+);
+
+// 7. Borrowed Books Summary (Using Aggregation)
+borrowRoutes.get(
+  "/",
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const borrowedBooks = await Borrow.aggregate([
+        {
+          $group: {
+            _id: "$book",
+            totalQuantity: { $sum: "$quantity" }
+          }
+        },
+        {
+          $lookup: {
+            from: "books",
+            localField: "_id",
+            foreignField: "_id",
+            as: "bookDetails"
+          }
+        },
+        { $unwind: "$bookDetails" },
+        {
+          $project: {
+            _id: 0,
+            book: {
+              title: "$bookDetails.title",
+              isbn: "$bookDetails.isbn"
+            },
+            totalQuantity: 1
+          }
+        }
+      ]);
+      res.send({
+        success: true,
+        message: "Borrowed books summary retrieved successfully",
+        data: borrowedBooks,
+      });
     } catch (error: any) {
       next(error);
     }
